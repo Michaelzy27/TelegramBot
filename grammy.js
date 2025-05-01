34c82e410268a3a0f3f5f4dfbc895490cb308167
Michaelzy27 <umehmichael27@gmail.com>
2025-05-01 10:13:06 +0100

    uodate

--- grammy.js
+++ grammy.js
@@ -1,26 +1,26 @@
 import { Bot } from "grammy"
 
-const bot = new Bot("7730958851:AAEkdTwAI53rERZIKZE78WEWvc7N98gANPo")
+const bot = new Bot("")
 const appUrl = 'https://app.soundrig.io/'
 
-bot.command('start', async (ctx) => {
-    const payload = ctx.message.text.split(' ')[1];
+// bot.command('start', async (ctx) => {
+//     const payload = ctx.message.text.split(' ')[1];
 
-    if(payload && payload.startsWith('ref_')) {
-        const refId = payload.replace('ref_', '')
-        //referUser(newUserId, refId)
-        console.log('user id is: ' + refId);
+//     if(payload && payload.startsWith('ref_')) {
+//         const refId = payload.replace('ref_', '')
+//         //referUser(newUserId, refId)
+//         console.log('user id is: ' + refId);
         
-    }
+//     }
 
-    ctx.reply('Welcome to SoundRig App: bridging the gap between fans and artists. Click the button below to launch the app!', {
-        reply_markup: {
-            inline_keyboard: [
-                [{text: 'Launch SoundRig!', web_app: { url: appUrl }}]
-            ]
-        }
-    })
-})
+//     ctx.reply('Welcome to SoundRig App: bridging the gap between fans and artists. Click the button below to launch the app!', {
+//         reply_markup: {
+//             inline_keyboard: [
+//                 [{text: 'Launch SoundRig!', web_app: { url: appUrl }}]
+//             ]
+//         }
+//     })
+// })
 
 bot.command('help', async (ctx) => {
     ctx.reply('SoundRig requires a Ton wallet. Simply launch the app and connect yuur wallet to begin.')
@@ -32,10 +32,20 @@ bot.command('contact', async (ctx) => {
 
 bot.on("message", (ctx) => {
     ctx.reply("Welcome to Soundrig")
-    ctx.replyWithInvoice("Test Payment", "I want to test this shit out", "{}", "XTR", [
-        { amount: 100, label: "Test Product" },
+    ctx.replyWithInvoice("Test Payment", "This is a test payment", "{}", "XTR", [
+        { amount: 1, label: "Test Product" },
       ])
 })
+
+// bot.api.setChatMenuButton({
+//     menu_button: {
+//         type: "web_app",
+//         text: "Launch App",
+//         web_app: {
+//             url: appUrl
+//         }
+//     }
+// })
 
 
 
@@ -54,33 +64,33 @@ function referUser() {
 
 // })
 
-const link = await bot.api.createInvoiceLink(
-    "Soundrig Stras Payment",
-    'Payment for Tun',
-    '{}',
-    '',
-    'XTR',
-    [ { amount: 1, label: "Test Product" }]
-)
+// const link = await bot.api.createInvoiceLink(
+//     "Soundrig Stras Payment",
+//     'Payment for Tun',
+//     '{}',
+//     '',
+//     'XTR',
+//     [ { amount: 1, label: "Test Product" }]
+// )
 
-console.log("payment link: " + link);
+// console.log("payment link: " + link);
 
 
 bot.on("pre_checkout_query", (ctx) => {
     //console.log("User X is about to buy tunez X");
-    ctx.answerPreCheckoutQuery(true)
+    ctx.answerPreCheckoutQuery(true);
     const payload = ctx.preCheckoutQuery.invoice_payload;
     const payloadParse = JSON.parse(payload)
     console.log(payloadParse);
     
 })
 
-bot.on("message", (ctx) => {
-    if(ctx.message.successful_payment) {
+bot.on("message:successful_payment", (ctx) => {
+    const payment = ctx.message?.successful_payment;
+    if(payment) {
         console.log("successful");
         
     }
-    ctx.succ
 })
 
 bot.start()
